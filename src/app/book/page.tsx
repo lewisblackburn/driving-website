10bb652795f77be5be8d6de977112eccdc0b6d93
lewisblackburn <lewisblackburn10@gmail.com>
2024-08-05 16:27:46 +0100

    feat(booking): changed t's and c's

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -21,7 +21,7 @@ export default function AboutPage() {
           title='Choose Your Course'
           description='Pick a course that suits you.'
         />
-        <div className='relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8'>
+        {/* <div className='relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8'>
           <h2 className='text-3xl font-bold tracking-tight text-black sm:text-4xl lg:text-5xl'>
             Terms and Conditions
           </h2>
@@ -40,6 +40,54 @@ export default function AboutPage() {
               your first driving lesson. If you fail to provide this proof, your
               instructor is entitled to refuse to conduct the driving lesson but
               may still charge you for such driving lesson.
+            </p>
+          </div>
+        </div> */}
+        <div className='relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8'>
+          <div className='container mx-auto px-4 py-8'>
+            <h1 className='text-3xl font-bold mb-4'>Terms and Conditions</h1>
+
+            <p className='mb-4'>
+              These terms and conditions outline the rules and regulations for
+              driving lessons with Drive 2 Learn.
+            </p>
+
+            <h2 className='text-2xl font-bold mb-2'>Requirements</h2>
+
+            <p className='mb-4'>In order to have driving lessons you must:</p>
+
+            <ul className='list-disc list-inside mb-4'>
+              <li>Be aged 17 or older (16 or over if disabled)</li>
+              <li>
+                Hold a driving licence that is valid for the UK (provisional,
+                full or an appropriate foreign licence)
+              </li>
+            </ul>
+
+            <p className='mb-4'>
+              It is your responsibility to provide your instructor with proof
+              that you have a valid licence to drive before the commencement of
+              your first driving lesson. If you fail to provide this proof, your
+              instructor is entitled to refuse to conduct the driving lesson but
+              may still charge you for such driving lesson.
+            </p>
+
+            <h2 className='text-2xl font-bold mb-2'>Paying For Lessons</h2>
+
+            <p className='mb-4'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro
+              doloribus vero dolore voluptatem ea, explicabo eum fuga optio in
+              eveniet ad repellendus magni quidem corporis? Aliquam optio
+              recusandae dicta voluptas.
+            </p>
+
+            <h3 className='text-xl font-semibold mb-2'>Refund Policy</h3>
+
+            <p className='mb-4'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro
+              doloribus vero dolore voluptatem ea, explicabo eum fuga optio in
+              eveniet ad repellendus magni quidem corporis? Aliquam optio
+              recusandae dicta voluptas.
             </p>
           </div>
         </div>
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,4 +1,5 @@
-import { Clock, MapPin, Share2 } from 'lucide-react';
+import { MailIcon, Share2 } from 'lucide-react';
+import Link from 'next/link';
 import * as React from 'react';
 import {
   RiFacebookFill,
@@ -11,25 +12,25 @@ import {
 export default function Banner() {
   return (
     <div className='flex flex-col space-y-3 lg:space-y-0 lg:flex-row items-center justify-between bg-foreground text-white px-5 xl:px-32 py-5'>
-      <div>
-        <div className='flex items-center space-x-2'>
-          <Clock className='text-primary' />
+      <Link href='/#contact'>
+        <div className='flex items-center space-x-3'>
+          <MailIcon className='text-primary' />
           <span className='font-bold uppercase text-xs tracking-widest'>
             Get In Touch
           </span>
         </div>
-      </div>
-      <div>
-        <div className='flex items-center space-x-2'>
+      </Link>
+      {/* <div>
+        <div className='flex items-center space-x-3'>
           <MapPin className='text-primary' />
           <span className='font-bold uppercase text-xs tracking-widest'>
             About Us
           </span>
         </div>
-      </div>
-      <div className='hidden lg:flex items-center space-x-2'>
-        <Share2 className='text-primary' />
-        <span className='font-bold uppercase text-xs tracking-widest'>
+      </div> */}
+      <div className='hidden lg:flex items-center'>
+        <Share2 className='text-primary mr-3' />
+        <span className='font-bold uppercase text-xs tracking-widest mr-2'>
           Social Media:
         </span>
         <div className='flex items-center space-x-3'>
